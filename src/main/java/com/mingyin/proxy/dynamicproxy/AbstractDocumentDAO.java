2eaa954576559c77db92b2a658fcca632b424ae8
zime <zime@163.com>
2019-09-24 13:21:32 +0800

    init

---
+++ src/main/java/com/mingyin/proxy/dynamicproxy/AbstractDocumentDAO.java
@@ -0,0 +1,6 @@
+package com.mingyin.proxy.dynamicproxy;
+
+//抽象DocumentDAO：抽象主题角色
+public interface AbstractDocumentDAO {
+	public Boolean deleteDocumentById(String documentId);
+}
---
+++ src/main/java/com/mingyin/proxy/dynamicproxy/AbstractUserDAO.java
@@ -0,0 +1,6 @@
+package com.mingyin.proxy.dynamicproxy;
+
+//抽象UserDAO：抽象主题角色
+public interface AbstractUserDAO {
+	public Boolean findUserById(String userId);
+}
